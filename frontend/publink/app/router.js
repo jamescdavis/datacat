d3f84eeefd266704e02552e5ad5c6788a20449cd
James C. Davis <James.Davis@tamucc.edu>
2017-02-20 13:27:37 -0600

    Added start of ember app for PubLink

---
+++ frontend/publink/app/router.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('datasets');
+  this.route('publications');
+});
+
+export default Router;
