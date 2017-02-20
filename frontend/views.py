a9ddcb5557fc488226f054457bd0881fd0e872fa
James C. Davis <James.Davis@tamucc.edu>
2017-02-20 13:10:15 -0600

    Added frontend index template and route for publink

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,10 @@
+from flask import render_template, send_from_directory
 from frontend import app
 
 @app.route('/')
 def index():
-    return 'DataCat'
+    return render_template('index.html')
+
+@app.route('/publink')
+def publink():
+    return send_from_directory(app.static_folder, 'publink/index.html')
